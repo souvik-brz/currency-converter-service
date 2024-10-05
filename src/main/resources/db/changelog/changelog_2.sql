6e20c2aad835b1ac0b7243c55ad3eb86b466473b
souvik-brz <souvik.brz@gmail.com>
2024-10-05 14:06:51 +0200

    added exceptions

---
+++ src/main/resources/db/changelog/changelog_2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS currency_converter_configs(
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    base_code VARCHAR(8) NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    frequency_in_minutes INT NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_at DATETIME NULL ON UPDATE CURRENT_TIMESTAMP
+);
